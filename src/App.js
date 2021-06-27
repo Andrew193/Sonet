df31b7775c9033764d0b7f364310ec5cd3263b84
Andrew Sadovinskiy <sadovinskiy@gmail.com>
2021-06-27 15:00:35 +0300

    Context api added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import { ToastContainer } from 'react-toastify';
 import { useRef, useState } from "react";
 import { toast } from 'react-toastify';
 import { io } from "socket.io-client";
+import Context from "./helpers/contextHelper"
+
+
 const sessionHelper = require("./helpers/sessionHelper")
 const socket = io();
 function App() {
@@ -22,53 +25,55 @@ function App() {
     setFlag(true)
   })
   return (
-    <div className="App">
-      {flag && <>
-        <Components.Header notify={notify} socket={socket} />
-        <Switch>
-          <Route exact path={"/"} render={() =>
-            <div className={"genContainer"}>
-              <Components.MainPage socket={socket} open={open} notify={notify} />
-              <Components.TopInfo socket={socket} />
-            </div>}>
-          </Route>
-          <Route exact path={"/auth"} render={() => <Components.ContainerAuth />}></Route>
-          <Route exact path={"/users/:id?"} render={() =>
-            <div className={"genContainer"}>
-              <Components.UsersContainer notify={notify}/>
-              <Components.TopInfo socket={socket} />
-            </div>}></Route>
-          <Route exact path={"/profile/:id?"} render={() =>
-            <div className={"genContainer"}>
-              <Components.Profile />
-              <Components.TopInfo socket={socket} />
-            </div>}></Route>
-          <Route exact path={"/posts/:id?"} render={() =>
-            <div className={"genContainer"}>
-              <Components.PostsContainer notify={notify} socket={socket} />
-              <Components.TopInfo socket={socket} />
-            </div>}></Route>
-          <Route exact path={"/post/:type"} render={() =>
-            <div className={"genContainer"}>
-              <Components.SpecialPosts notify={notify} socket={socket} />
-            </div>}>
-          </Route>
-          <Route exact path={"/comment/:id?"} render={() =>
-            <Components.Comment notify={notify} socket={socket} />
-          }>
-          </Route>
-          <Route exact path={"/followers"} render={() =>
-            <Components.Followers />
-          }>
-          </Route>
-        </Switch>
-        <Components.Footer />
-        <ToastContainer toastStyle={{ background: "black", borderRadius: "15px" }} hideProgressBar={true} autoClose={2000}
-          position="top-right" />
-        <Components.ModalUser ref={modal} click={() => open()} />
-      </>}
-      <Route exact path={"/auth"} render={() => <Components.ContainerAuth />}></Route>
-    </div>
+    <Context.Provider value={{ notify, socket }}>
+      <div className="App">
+        {flag && <>
+          <Components.Header notify={notify} socket={socket} />
+          <Switch>
+            <Route exact path={"/"} render={() =>
+              <div className={"genContainer"}>
+                <Components.MainPage socket={socket} open={open} notify={notify} />
+                <Components.TopInfo socket={socket} />
+              </div>}>
+            </Route>
+            <Route exact path={"/auth"} render={() => <Components.ContainerAuth />}></Route>
+            <Route exact path={"/users/:id?"} render={() =>
+              <div className={"genContainer"}>
+                <Components.UsersContainer notify={notify} />
+                <Components.TopInfo socket={socket} />
+              </div>}></Route>
+            <Route exact path={"/profile/:id?"} render={() =>
+              <div className={"genContainer"}>
+                <Components.Profile />
+                <Components.TopInfo socket={socket} />
+              </div>}></Route>
+            <Route exact path={"/posts/:id?"} render={() =>
+              <div className={"genContainer"}>
+                <Components.PostsContainer notify={notify} socket={socket} />
+                <Components.TopInfo socket={socket} />
+              </div>}></Route>
+            <Route exact path={"/post/:type"} render={() =>
+              <div className={"genContainer"}>
+                <Components.SpecialPosts notify={notify} socket={socket} />
+              </div>}>
+            </Route>
+            <Route exact path={"/comment/:id?"} render={() =>
+              <Components.Comment notify={notify} socket={socket} />
+            }>
+            </Route>
+            <Route exact path={"/followers"} render={() =>
+              <Components.Followers />
+            }>
+            </Route>
+          </Switch>
+          <Components.Footer />
+          <ToastContainer toastStyle={{ background: "black", borderRadius: "15px" }} hideProgressBar={true} autoClose={2000}
+            position="top-right" />
+          <Components.ModalUser ref={modal} click={() => open()} />
+        </>}
+        <Route exact path={"/auth"} render={() => <Components.ContainerAuth />}></Route>
+      </div>
+    </Context.Provider>
   );
 }
 
