81c785aa312dd35928ab7dd9562da70f57cafd30
Andrew Home <sadovinskiy2@gmail.com>
2022-06-18 17:28:24 +0300

    Bring back image

--- src/chats/Message.jsx
+++ src/chats/Message.jsx
@@ -14,7 +14,7 @@ function Message(props) {
                     src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                     alt=""
                 />
-                <p className="messageText">{message.text}</p>
+                <p className="messageText">{message.text || message?.messageText}</p>
             </div>
             <div className="messageBottom">{message.createdAt}</div>
         </div>
